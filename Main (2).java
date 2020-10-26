f3d72a9aad88042fef4a10130db9d157009487c9
jl69135 <69662401+jl69135@users.noreply.github.com>
2020-10-25 21:01:02 -0700

    Add files via upload

---
+++ Main (2).java
@@ -0,0 +1,42 @@
+public class Main
+{
+	public static void main(String[] args) {
+		System.out.println("_Using relational operators_");
+		boolean a = true;
+		boolean b = false;
+		System.out.println(a != b);
+		//Output is true
+		
+		int j = 5;
+		Integer l = new Integer(5);
+		System.out.print(j == l);
+		//Integer object being compared to an int
+		//Output is true because java unpacks the int out of the Integer bbject
+	    
+	    int h = 5;
+	    double i = 5.0;
+	    System.out.print(h == i);
+	    //int 5 can be compared to double 5.0
+	    //Output is true (int can be compared to double but sometimes NOT vice versa)
+	    
+	    int t = 5;
+	    Double r = new Double(5.0);
+	    System.out.print(t == r);
+	    //Output is true (Java unpacks the Double object, compares 5.0 to 5)
+	    
+	    Integer m = new Integer(5);
+	    Integer y = new Integer(5);
+	    System.out.print(m == y);
+	    //Output is false, two obejct are being compared (not the same object)
+	    
+	    Integer g = new Integer(5);
+	    Double n = new Double(5);
+	    System.out.print(g == n);
+	    //Output is false, cannot compare two different objects
+	    
+	    System.out.println("Cannot compare two different objects. If one object refers to the same object, then comparable.");
+	    System.out.println(">, >=, <=, < cannot be used on these types of object references: c1 = Jim, c2 = Bob");
+	    System.out.println("Also cannot do these signs on Strings or binary operators like true > false.");
+	    System.out.println("These relational operators have lower precedence than arithmetic operators.");
+	}
+}
